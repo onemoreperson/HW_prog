a41e1cb4da47f40e28f59b1dd85ed1baa3c51cbb
onemoreperson <b.olgusik@mail.ru>
2017-03-28 02:55:03 +0300

    Update HW6.03 Брускова.py

--- HW6.03 Брускова.py
+++ HW6.03 Брускова.py
@@ -14,6 +14,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import tree
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from nltk import word_tokenize, wordpunct_tokenize
+from sklearn.ensemble import RandomForestClassifier
 
 tknzr = TweetTokenizer()
 st = EnglishStemmer()
@@ -24,104 +27,113 @@ db0 = pd.read_csv('SMSSpamCollection', sep='\t', names = ['A','B'])#вообще
 db0['A'] = db0['A'].map({'spam': 0, 'ham':1}).astype(int)
 x1 = db0['A']
 y1 = db0['B']
-db0['C'] = range(len(x1))
 
-#------------------------------------------------------------------------
-#xtrain, xtest, ytrain, ytest = train_test_split(x1, y1)
-#
-#clf = DummyClassifier(constant=None, strategy='most_frequent',random_state=0,)
-#clf.fit(xtrain, ytrain)
-#a = clf.score(xtest, ytest) 
-#
-#print(a)
 
-#------------------------------------------------------------------------
+a = db0[db0['A']==0]
+b = db0[db0['A']==1]
 
-idx = 0   #деление выборки на равные части
-for item in x1:
-    idx += 1
-num = [idx for idx, item in enumerate(x1) if item == 0]
-num1 = [idx for idx, item in enumerate(x1) if item == 1]
-summ = [i for i in num[:740]] + [i1 for i1 in num1[:740]]
+summ = [i for i in a['C'][:300]] + [i1 for i1 in b['C'][:300]]# делаем сбалансированную выборку
+#в нее войдет равное кол-во примеров
+db = pd.DataFrame(db0, index = sorted(summ))# по 50% спама и хама благодаря массиву 
+
+x = db['B']
+y = db['A']
+
 
 db = pd.DataFrame(db0, index = sorted(summ))# по 50% спама и хама    
 x = db['A']
 y = db['B']
 
 
-####для стемов и лемматизации
-#def lem(y): 
-#    y = re.sub(r'\s+', ' ', str(y))
+####для стемов и лемматизации в векторах, здесь все шло методом перебора, поэтому результаты внизу
+def lem(y): 
+    y = re.sub(r'\s+', ' ', str(y))
 ##    y = y.lower()
-#    tok = tknzr.tokenize(y)
-#    lem = [wnl.lemmatize(i) for i in tok]
-#    return( " ".join(lem)) 
-#n = y.size
-#wer = [lem(y[i]) for i in sorted(summ)]
+    tok = tknzr.tokenize(y)
+    lem = [wnl.lemmatize(i) for i in tok]
+    return( " ".join(lem)) 
 
-#def ste(y):  
-#    y = re.sub(r'\s+', ' ', str(y))
+
+def ste(y):  
+    y = re.sub(r'\s+', ' ', str(y))
 ##    y = y.lower()
-#    tok = tknzr.tokenize(y)
-#    ste = [st.stem(i) for i in tok]
-#    return( " ".join(ste))   
-#n = y.size
-#wer = [ste(y[i]) for i in sorted(summ)]
-#
-#db['C'] = wer #  снять при выборе одной из 2х предыдущих функций, чтобы у У учитывались изменения
-#y = db['C']
+    tok = tknzr.tokenize(y)
+    ste = [st.stem(i) for i in tok]
+    return( " ".join(ste))   
 
-xtrain, xtest, ytrain, ytest = train_test_split(x, y)
-vectorizer = CountVectorizer(analyzer = "word",   
-                             tokenizer = None)    
+
+xtrain, xtest, ytrain, ytest = train_test_split(list(x), np.array(y))
+clf = CountVectorizer(analyzer = "word",   
+                             tokenizer = None,   
 #                             stop_words = 'english', 
-#                             token_pattern = '[^a-zA-Z0-9]')
-#                             min_df = 0.3) 
-vectorizer.fit_transform(y)
-vocab = vectorizer.get_feature_names()
-data = vectorizer.transform(y)
+#                             token_pattern = '[^a-zA-Z0-9]',
+                             max_df = 1.0) 
 
-naive_model = MultinomialNB()
-naive_model.fit(data, db['A'])
-cv_results = cross_val_score(naive_model, data, db['A'], cv=10, scoring='accuracy')
-print(cv_results.mean(), cv_results.std())
-#
 transformer = TfidfVectorizer(analyzer = "word",                             
-                                tokenizer = None)    
+                                tokenizer = None,    
 #                                stop_words = 'english', 
-#                                token_pattern = '[^a-zA-Z0-9]')
-#                                min_df = 0.3)
-vectorizer.fit_transform(y)
-vocab = vectorizer.get_feature_names()
-data1 = vectorizer.transform(y)
+#                                token_pattern = '[^a-zA-Z0-9]',
+                                max_df = 1.0)
+
+vectorizer = clf.fit_transform(xtrain)
+data = clf.transform(xtest)
+
 naive_model = MultinomialNB()
-naive_model.fit(data1, db['A'])
-cv_results = cross_val_score(naive_model, data1, db['A'], cv=10, scoring='accuracy')
-print(cv_results.mean(), cv_results.std())
+cv_results = naive_model.fit(vectorizer, ytrain).predict(data)
 
-# результаты по векторам: count: 0.958108108108(первый) 0.0141085223093(второй)
-#tfidf 0.958108108108 0.0141085223093
-# stop_words - влияния не обнаружено.
-#  token_pattern - влияет на числа. В первом уменьшает на 0.15, во втором увеличивает на 0.005.
-# min_df на 0.4 хуже в первом, на 0.2 лучше во втором.
-# max_df - влияния не обнаружено.
-# отсутствие лемматизации и стемминга - высокий результат в 1 случае, маленький во втором
-#lemmatize, stem серьезных различий нет, ниже исходного уровня. Лемматицация дает незначительно больший результат.
+#посмотрим результаты по метрикам разных векторов
+print('Bayes CV')
+print(classification_report(ytest, cv_results, target_names=['S', 'H']))
 
+vec = transformer.fit_transform(xtrain)
+data1 = transformer.transform(xtest)
+cv_results1 = naive_model.fit(vec, ytrain).predict(data1)
+print('Bayes TF')
+print(classification_report(ytest, cv_results1, target_names=['S', 'H']))
+# 
+#в большинстве случаев тфидф работает хуже, только лучший результат вышел одинаково на обоих векторах
+#самые высокие результаты дает параметр max_df при значении 0.5
+#лемматизация и стемизация мало отличаются друг от друга
+#если считать знаки препинания отдельными токенами - результат выше
+#стоп слова мало что меняют
+#
+#
 #------------------------------2-----------------------------------------
 
-#for t in range(1,100):
-#    clf= tree.DecisionTreeClassifier(random_state= None, max_features= None, 
-#                                     class_weight= None, 
-#                                     max_leaf_nodes= None, 
-#                                     min_samples_leaf= 1, criterion= 'gini', 
-#                                     splitter= 'best', min_weight_fraction_leaf= 0.0, 
-#                                     min_samples_split= 2, max_depth= None, 
-#                                     presort= False, min_impurity_split= 1e-07)
-#
-#    clf = clf.fit(xtrain, ytrain)
-##    clf = clf.get_params(deep=True)
-#
-#    ypred = clf.predict(xtest)
-#cv_results = cross_val_score(clf, clf, db["A"], cv=10, scoring='accuracy')
-#print(cv_results.mean(), cv_results.std())
+
+tr= tree.DecisionTreeClassifier(random_state= None, max_features= None, 
+                                     class_weight= None, 
+                                     max_leaf_nodes= None, 
+                                     min_samples_leaf= 1, criterion= 'gini', 
+                                     splitter= 'best', min_weight_fraction_leaf= 0.0, 
+                                     min_samples_split= 2, max_depth= None, 
+                                     presort= False, min_impurity_split= 1e-07)
+
+cv_results3 = tr.fit(vectorizer, ytrain).predict(data)
+print('tree CV')
+print(classification_report(ytest, cv_results3, target_names=['S', 'H']))
+
+print('tree TF')
+cv_results4 = tr.fit(vec, ytrain).predict(data1)
+print(classification_report(ytest, cv_results4, target_names=['S', 'H']))
+
+
+rfc = RandomForestClassifier(bootstrap=True, min_impurity_split=1e-07, 
+                                 n_estimators=10, verbose=0,
+                                 max_leaf_nodes= None, oob_score=False, 
+                                 min_samples_leaf=1,
+                                 class_weight=None, max_features='auto', 
+                                 max_depth=None, min_samples_split=2,
+                                 random_state=None, 
+                                 min_weight_fraction_leaf=0.0, warm_start=False,
+                                 criterion='gini', n_jobs=1)
+
+cv_results5 = rfc.fit(vectorizer, ytrain).predict(data)
+print('tree CV')
+print(classification_report(ytest, cv_results5, target_names=['S', 'H']))
+
+print('tree TF')
+cv_results6 = rfc.fit(vec, ytrain).predict(data1)
+print(classification_report(ytest, cv_results6, target_names=['S', 'H']))
+
+# лучший вариант дает байесовский классификатор при CountVectorize
